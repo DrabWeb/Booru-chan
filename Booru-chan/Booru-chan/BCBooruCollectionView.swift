57c69537fb32a60bd8e3d4cf939ee006ea326863
DrabWeb <sethunity@gmail.com>
2016-05-05 19:53:23 -0300

    Added right click menus to Booru browser items. You can open them in the browser, copy their URL or copy their image URL

---
+++ Booru-chan/Booru-chan/BCBooruCollectionView.swift
@@ -0,0 +1,18 @@
+//
+//  BCBooruCollectionView.swift
+//  Booru-chan
+//
+//  Created by Seth on 2016-05-05.
+//  Copyright © 2016 DrabWeb. All rights reserved.
+//
+
+import Cocoa
+
+class BCBooruCollectionView: NSCollectionView {
+    
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+}
--- Booru-chan/Booru-chan/BCBooruCollectionViewCollectionViewItem.swift
+++ Booru-chan/Booru-chan/BCBooruCollectionViewCollectionViewItem.swift
@@ -10,6 +10,49 @@ import Cocoa
 
 class BCBooruCollectionViewCollectionViewItem: NSCollectionViewItem {
     
+    override func rightMouseDown(theEvent: NSEvent) {
+        /// The menu to show when the user right clicks this item
+        let menu : NSMenu = NSMenu();
+        
+        // Deselect all the items in the Booru collection view
+        self.collectionView.deselectAll(self);
+        
+        // Select this item
+        self.selected = true;
+        
+        // Add the menu items to the menu
+        menu.addItemWithTitle("Open In Browser", action: Selector("openInBrowser"), keyEquivalent: "");
+        menu.addItemWithTitle("Copy URL", action: Selector("copyUrlToClipboard"), keyEquivalent: "");
+        menu.addItemWithTitle("Copy Image URL", action: Selector("copyImageUrlToClipboard"), keyEquivalent: "");
+        
+        // Show the menu
+        NSMenu.popUpContextMenu(menu, withEvent: theEvent, forView: self.view);
+    }
+    
+    /// Opens this post in the browser
+    func openInBrowser() {
+        // Open this item's post's URL in the browser
+        NSWorkspace.sharedWorkspace().openURL(NSURL(string: (self.representedObject as! BCBooruCollectionViewItem).representedPost!.url)!);
+    }
+    
+    /// Copys the URL of this post to the clipboard
+    func copyUrlToClipboard() {
+        // Add the string type to the general pasteboard
+        NSPasteboard.generalPasteboard().declareTypes([NSStringPboardType], owner: nil);
+        
+        // Set the string of the general pasteboard to this item's post's URL
+        NSPasteboard.generalPasteboard().setString((self.representedObject as! BCBooruCollectionViewItem).representedPost!.url, forType: NSStringPboardType);
+    }
+    
+    /// Copys the URL of this post to the clipboard
+    func copyImageUrlToClipboard() {
+        // Add the string type to the general pasteboard
+        NSPasteboard.generalPasteboard().declareTypes([NSStringPboardType], owner: nil);
+        
+        // Set the string of the general pasteboard to this item's post's image's URL
+        NSPasteboard.generalPasteboard().setString((self.representedObject as! BCBooruCollectionViewItem).representedPost!.imageUrl, forType: NSStringPboardType);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
