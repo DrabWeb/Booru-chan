cb072904fba438c0e3fa72d4f16d74a1299ff29c
DrabWeb <sethunity@gmail.com>
2016-05-06 09:53:27 -0300

    Made copy/paste from the search field work properly

--- Booru-chan/Booru-chan/BCBooruSearchTokenField.swift
+++ Booru-chan/Booru-chan/BCBooruSearchTokenField.swift
@@ -38,6 +38,37 @@ class BCBooruSearchTokenField: BCAlwaysActiveTokenField, NSTokenFieldDelegate {
         }
     }
     
+    func tokenField(tokenField: NSTokenField, readFromPasteboard pboard: NSPasteboard) -> [AnyObject]? {
+        // Return the text from pboard split at every space
+        return (pboard.stringForType(NSStringPboardType)?.componentsSeparatedByString(" "));
+    }
+    
+    func tokenField(tokenField: NSTokenField, writeRepresentedObjects objects: [AnyObject], toPasteboard pboard: NSPasteboard) -> Bool {
+        // Add the string type to the pasteboard
+        pboard.declareTypes([NSStringPboardType], owner: nil);
+        
+        /// The string to paste to the pasteboard
+        var pasteString : String = "";
+        
+        // For every string in objects...
+        for(_, currentString) in (objects as! [String]).enumerate() {
+            // Add the current string with a trailing space to pasteString
+            pasteString += currentString + " ";
+        }
+        
+        // If pasteString isnt empty...
+        if(pasteString != "") {
+            // Remove the final trailing space from pasteString
+            pasteString = pasteString.substringToIndex(pasteString.endIndex.predecessor());
+        }
+        
+        // Paste pasteString to the pasteboard
+        pboard.setString(pasteString, forType: NSStringPboardType);
+        
+        // Always allow copying tokens
+        return true;
+    }
+    
     func tokenField(tokenField: NSTokenField, shouldAddObjects tokens: [AnyObject], atIndex index: Int) -> [AnyObject] {
         // Empty lastDownloadedTags
         lastDownloadedTags = [];
