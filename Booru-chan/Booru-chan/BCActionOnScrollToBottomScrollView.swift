750068c94e745794cd8a41cd4dc0b95e16ed5b63
DrabWeb <sethunity@gmail.com>
2016-10-27 22:44:11 -0300

    Fixed scrolling to go to the next page not working sometimes, like when the view is too big to scroll down

--- Booru-chan/Booru-chan/BCActionOnScrollToBottomScrollView.swift
+++ Booru-chan/Booru-chan/BCActionOnScrollToBottomScrollView.swift
@@ -18,8 +18,8 @@ class BCActionOnScrollToBottomScrollView: NSScrollView {
     /// The selector to call when the user reaches the bottom of this scroll view
     var reachedBottomAction : Selector? = nil;
     
-    override func reflectScrolledClipView(cView: NSClipView) {
-        super.reflectScrolledClipView(cView);
+    override func scrollWheel(theEvent: NSEvent) {
+        super.scrollWheel(theEvent);
         
         // If we are at the bottom of the scroll view...
         if(self.documentView?.frame.height <= ((self.documentVisibleRect.height + self.documentVisibleRect.origin.y + self.contentInsets.top + self.contentInsets.bottom))) {
@@ -35,10 +35,34 @@ class BCActionOnScrollToBottomScrollView: NSScrollView {
             // Say we are at the bottom
             atBottom = true;
         }
-        // If we arent at the bottom of the scroll view
+            // If we arent at the bottom of the scroll view
         else {
             // Say we arent at the bottom
             atBottom = false;
         }
     }
+    
+//    override func reflectScrolledClipView(cView: NSClipView) {
+//        super.reflectScrolledClipView(cView);
+//        
+//        // If we are at the bottom of the scroll view...
+//        if(self.documentView?.frame.height <= ((self.documentVisibleRect.height + self.documentVisibleRect.origin.y + self.contentInsets.top + self.contentInsets.bottom))) {
+//            // If we havent already called the bottom reached action...
+//            if(!atBottom) {
+//                // If reachedBottomTarget and reachedBottomAction are both not nil...
+//                if(reachedBottomTarget != nil && reachedBottomAction != nil) {
+//                    // Call the reached bottom action
+//                    reachedBottomTarget!.performSelector(reachedBottomAction!);
+//                }
+//            }
+//            
+//            // Say we are at the bottom
+//            atBottom = true;
+//        }
+//        // If we arent at the bottom of the scroll view
+//        else {
+//            // Say we arent at the bottom
+//            atBottom = false;
+//        }
+//    }
 }
