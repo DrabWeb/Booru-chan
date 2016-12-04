0f81d0b98d22b33dbecd4525ff824a9a06cd33f7
DrabWeb <sethunity@gmail.com>
2016-12-04 15:18:01 -0400

    Fixed a bug where if you dragged the window by the titlebar where the split view was it would resize the split view and not drag

---
+++ Booru-chan/Booru-chan/BCTitlebarView.swift
@@ -0,0 +1,22 @@
+//
+//  BCTitlebarView.swift
+//  Booru-chan
+//
+//  Created by Ushio on 12/4/16.
+//
+
+import Cocoa
+
+class BCTitlebarView: NSVisualEffectView {
+
+    // Cancel out the mouse triggering views behind the titlebar
+    
+    override func mouseDown(with event: NSEvent) {
+        // Do nothing
+    }
+    
+    override func mouseDragged(with event: NSEvent) {
+        // Drag the window
+        self.window?.performDrag(with: event);
+    }
+}
