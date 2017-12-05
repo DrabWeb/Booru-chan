23cdde26673546036e1324b915c70c3673c319c3
DrabWeb <sethunity@gmail.com>
2017-12-05 19:42:39 -0400

    Info bar label.

--- Booru-chan/Booru-chan/BrowserController.swift
+++ Booru-chan/Booru-chan/BrowserController.swift
@@ -8,21 +8,21 @@
 import Cocoa
 
 class BrowserController: NSSplitViewController, IThemeable {
-    @IBOutlet weak var postsItem: NSSplitViewItem!
+    @IBOutlet private weak var postsItem: NSSplitViewItem!
     private var postsController: PostsController {
         get {
             return postsItem.viewController as! PostsController;
         }
     }
 
-    @IBOutlet weak var infoBarItem: NSSplitViewItem!
+    @IBOutlet private weak var infoBarItem: NSSplitViewItem!
     private var infoBarController: InfoBarController {
         get {
             return infoBarItem.viewController as! InfoBarController;
         }
     }
 
-    @IBOutlet weak var tagListItem: NSSplitViewItem!
+    @IBOutlet private weak var tagListItem: NSSplitViewItem!
     private var tagListController: TagListController {
         get {
             return tagListItem.viewController as! TagListController;
--- Booru-chan/Booru-chan/InfoBarController.swift
+++ Booru-chan/Booru-chan/InfoBarController.swift
@@ -8,5 +8,36 @@
 import Cocoa
 
 class InfoBarController: NSViewController {
+    @IBOutlet private weak var label: NSTextField!
 
+    var imageSize: NSSize! {
+        didSet {
+            updateLabel();
+        }
+    }
+
+    var rating: Rating! {
+        didSet {
+            updateLabel();
+        }
+    }
+
+    var loadingProgress: Float! {
+        didSet {
+            updateLabel();
+        }
+    }
+
+    private func updateLabel() {
+        if imageSize == nil && self.rating == nil && loadingProgress == nil {
+            label.stringValue = "";
+            return;
+        }
+
+        let size = imageSize == nil ? "?x?" : "\(Int(imageSize.width))x\(Int(imageSize.height))";
+        let rating = self.rating == nil ? "?" : "\(String(String(describing: self.rating!).first!).uppercased())";
+        let progress = loadingProgress == nil ? "?" : "\(Int((loadingProgress * 100).rounded()))";
+
+        label.stringValue = "\(size) [\(rating)] \(progress)%";
+    }
 }
