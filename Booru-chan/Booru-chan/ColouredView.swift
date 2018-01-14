d75b5102726ce586d8383a187404ef86d096c4f3
DrabWeb <sethunity@gmail.com>
2018-01-14 17:49:58 -0400

    Cleanup ColouredView.swift

--- Booru-chan/Booru-chan/ColouredView.swift
+++ Booru-chan/Booru-chan/ColouredView.swift
@@ -8,18 +8,13 @@
 import Cocoa
 
 class ColouredView: NSView {
-    
-    /// The background color of the view
-    var backgroundColor : NSColor = NSColor.clear;
+
+    var colour: NSColor = NSColor.clear;
 
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
 
-        // Drawing code here.
-        // Make sure we get a layer
         self.wantsLayer = true;
-        
-        // Set the layer's background color
-        self.layer?.backgroundColor = backgroundColor.cgColor;
+        self.layer?.backgroundColor = colour.cgColor;
     }
 }
