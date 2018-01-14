a04f76ec42589b7bdf22919c5f597cd3c5103671
DrabWeb <sethunity@gmail.com>
2018-01-14 17:49:20 -0400

    Cleanup NoDividerSplitView.swift

--- Booru-chan/Booru-chan/NoDividerSplitView.swift
+++ Booru-chan/Booru-chan/NoDividerSplitView.swift
@@ -8,15 +8,7 @@
 import Cocoa
 
 class NoDividerSplitView: NSSplitView {
-    
-    // Override the divider thickness to 0
-    override var dividerThickness : CGFloat {
+    override var dividerThickness: CGFloat {
         return 0;
-    }
-
-    override func draw(_ dirtyRect: NSRect) {
-        super.draw(dirtyRect)
-
-        // Drawing code here.
     }
 }
