b57e960d09391e406fe4c3e63627f3b7913f4d30
DrabWeb <sethunity@gmail.com>
2018-01-14 17:54:29 -0400

    Cleanup Extensions.swift

--- Booru-chan/Booru-chan/Extensions.swift
+++ Booru-chan/Booru-chan/Extensions.swift
@@ -9,26 +9,24 @@
 import Cocoa
 
 extension NSTokenField {
-    /// All the tokens that are currently entered in the field
-    var tokens : [String] {
-        // Return the tokens
-        if(self.tokenStyle == .none) {
-            return self.stringValue.components(separatedBy: " ");
-        }
-        else {
-            return self.stringValue.components(separatedBy: ",");
+
+    var tokens: [String] {
+        switch tokenStyle {
+            case .none:
+                return self.stringValue.components(separatedBy: " ");
+            default:
+                return self.stringValue.components(separatedBy: ",");
         }
     }
 
-    /// Adds the given token from this token field's tokens
     func addToken(_ token : String) {
-        if(self.tokenStyle == .none) {
-            // Add the given token to this fields tokens
-            self.stringValue += " " + token;
-        }
-        else {
-            // Add the given token to this fields tokens
-            self.stringValue += "," + token;
+        switch tokenStyle {
+            case .none:
+                self.stringValue += " \(token)";
+                return;
+            default:
+                self.stringValue += ",\(token)";
+                return;
         }
     }
 }
